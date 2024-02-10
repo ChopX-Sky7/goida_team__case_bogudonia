b53cb24e83ea8d78644ac150667599868488ed3c
Dmitry Yaschenko <shrikelionel0@gmail.com>
2024-02-11 01:10:08 +0300

    Initial commit

---
+++ src/test/java/com/azovsea/hackathon/ProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.azovsea.hackathon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
